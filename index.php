dcb7b6e55cd19302073bd852aef960ffdcd25e71
sailijar <sailijar88@gmail.com>
2025-09-03 17:50:57 +0700

    membuat tampilan mengunakan bootstrap dan membuat koneksi

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include "koneksi.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <title>Klinik</title>
+</head>
+<body>
+  <?php
+include "navbar.php";
+?>
+
+  <div class="container">
+     <!-- disini kontennya -->
+    <div class="row">
+       <div class="col-6 m-auto mt-5">
+        <div class="card">
+          <div class="card-header">
+              Klinik Sehat
+          </div>
+            <div class="card-body">
+              <h5 class="card-title">Halo,</h5>
+                <p class="card-text">Selamat Datang di Aplikasi Klinik Sehat</p>
+                </div>
+          </div>
+      </div>
+    </div>
+
+  </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
